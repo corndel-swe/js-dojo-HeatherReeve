ebca05ee85669bf7d008c5bb179529951cbe7111
heather Reeve <heather.reeve@scope.org.uk>
2024-06-13 16:10:02 +0100

    completed

--- arrays/14.js
+++ arrays/14.js
@@ -7,6 +7,13 @@
  * appendYear(['Book One', 'Book Two'], 2024); // returns ['Book One (2024)', 'Book Two (2024)']
  */
 export function appendYear(titles, year) {
+  function addTitleAndYear(title) {
+    return title + ' (' + year + ')'
+  }
+  
   // TODO: Use the map() method to append the year to each title
   // TODO: Return the new array with the year appended to each title
+  return titles.map (addTitleAndYear)
 }
+
+
